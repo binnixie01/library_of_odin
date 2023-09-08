980b64a2d094e925a40962264f6fb3081da0004b
Thangjam Binson <binsonsingh@gmail.com>
2023-09-08 08:18:49 +0530

    Book object and input

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # library_of_odin
+
+This is a library project for Odin project.
+I will update this project as I go along the curriculum
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const myLibrary = []
+function Book(name,author,pages) {
+    this.name=name;
+    this.author=author;
+    this.pages=pages;
+    
+}
+
+function addBook() {
+    let name = prompt("enter name")
+    let author=prompt("enter author")
+    let pages=prompt("enter pages")
+    myLibrary.push(new Book(name,author,pages))
+    
+}
+addBook()
