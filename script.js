1f7f2f661b2db8eb36e31e2730ca0cec2b89cf31
Thangjam Binson <binsonsingh@gmail.com>
2023-09-09 10:11:36 +0530

    library book on page shown

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const myLibrary = []
-function Book(name,author,pages) {
-    this.name=name;
-    this.author=author;
-    this.pages=pages;
-    
+const myLibrary = [
+  { name: "12 Rules for Life", author: "Jordan Peterson", pages: 200 },
+];
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
 }
 
 function addBook() {
-    let name = prompt("enter name")
-    let author=prompt("enter author")
-    let pages=prompt("enter pages")
-    myLibrary.push(new Book(name,author,pages))
+  let name = prompt("enter name");
+  let author = prompt("enter author");
+  let pages = prompt("enter pages");
+  myLibrary.push(new Book(name, author, pages));
+  add()
+}
+
+function add() {myLibrary.forEach((element) => {
+    document.getElementById("container").innerHTML = `<div><div>${element.name}</div><div>${element.author}</div><div>${element.pages}</div></div>`
+    })
     
 }
-addBook()
+
+myLibrary.forEach((element) => {
+document.getElementById("container").innerHTML = `<div><div>${element.name}</div><div>${element.author}</div><div>${element.pages}</div></div>`
+})
